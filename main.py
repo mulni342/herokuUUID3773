56a1318503c6f8fe8a6ce0755e8bfeb29e0db013
mulni342 <75677021+mulni342@users.noreply.github.com>
2021-08-18 20:21:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from psutil import virtual_memory
-
-mem = virtual_memory()
-print(mem.total)
+import psutil
+import time 
 
 
-
+while True:
+  print(psutil.cpu_percent())
+  print(psutil.virtual_memory())  # physical memory usage
+  print('memory % used:', psutil.virtual_memory()[2])
+  time.sleep(3)
